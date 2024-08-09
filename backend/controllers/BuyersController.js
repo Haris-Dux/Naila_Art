073b5301a54943049443ebb89f57d460339650a8
Haris <HarisSaeed583@gmail.com>
2024-08-10 01:08:36 +0500

    updating  generated buyer

--- backend/controllers/BuyersController.js
+++ backend/controllers/BuyersController.js
@@ -49,7 +49,7 @@ export const generateBuyersBillandAddBuyer = async (req, res, next) => {
       ];
       const missingFields = [];
       requiredFields.forEach((field) => {
-        if (!req.body[field] === undefined || !req.body[field] === null) {
+        if (req.body[field] === undefined || req.body[field] === null) {
           missingFields.push(field);
         } else if (field === "suits_data") {
           if (suits_data.length < 1)
@@ -131,14 +131,10 @@ export const generateBuyersBillandAddBuyer = async (req, res, next) => {
         todayBuyerDebit: (dailySaleForToday.saleData.todayBuyerDebit +=
           remaining),
         totalProfit: (dailySaleForToday.saleData.totalProfit += TotalProfit),
+        totalCash:dailySaleForToday.saleData.totalCash += paid
       };
 
-      if (payment_Method === "cashSale") {
-        dailySaleForToday.saleData.totalCash += paid;
-      }
-
       dailySaleForToday.saleData = updatedSaleData;
-      console.log(updatedSaleData);
       await dailySaleForToday.save({ session });
 
       //DATA FOR VIRTUAL ACCOUNT
@@ -281,5 +277,248 @@ export const getBuyerById = async (req,res,next) => {
     return res.status(500).json({error:error.message})
   }
 };
+
+export const validateD_NoAndGetSuitData = async (req,res,next) => {
+  try {
+    const {d_no} = req.body;
+    if (!d_no) throw new Error("D No Required");
+    const suitData = await SuitsModel.find({d_no});
+    if(!suitData) throw new Error("Suit Data Not Found");
+    setMongoose();
+    return res.status(200).json(suitData);
+  } catch (error) {
+    return res.status(500).json({error:error.message})
+  }
+};
+
+export const validateAndGetOldBuyerData = async (req,res,next) => {
+  try {
+    const {name} = req.body;
+    if(!name) throw new Error('Buyer Name Required');
+    const oldBuyerData = await BuyersModel.find({name:{$regex:name,$options:'i'}});
+    if(!oldBuyerData) throw new Error('No Data Found With This Buyer Name');
+    setMongoose();
+    return res.status(200).json({success:true,oldBuyerData});
+  } catch (error) {
+    return res.status(500).json({error:error.message});
+  }
+}
+
+export const generateBillForOldbuyer = async (req,res,nex) => {
+  const session = await mongoose.startSession();
+  try {
+    await session.withTransaction(async () => {
+      const {
+        buyerId,
+        branchId,
+        serialNumber,
+        name,
+        city,
+        cargo,
+        phone,
+        date,
+        bill_by,
+        payment_Method,
+        packaging,
+        discount,
+        suits_data,
+        total,
+        paid,
+        remaining,
+      } = req.body;
+
+      //VALIDATING FIELDS DATA
+      const requiredFields = [
+        "buyerId",
+        'branchId',
+        "serialNumber",
+        "name",
+        "city",
+        "cargo",
+        "phone",
+        "date",
+        "bill_by",
+        "payment_Method",
+        "packaging",
+        "discount",
+        "suits_data",
+        "total",
+        "paid",
+        "remaining",
+      ];
+      const missingFields = [];
+      requiredFields.forEach((field) => {
+        if (req.body[field] === undefined || req.body[field] === null) {
+          missingFields.push(field);
+        } else if (field === "suits_data") {
+          if (suits_data.length < 1)
+            throw new Error("Please Add Atleat 1 Suit");
+          req.body.suits_data.forEach((suit, index) => {
+            const requiredSuitFields = [
+              "id",
+              "d_no",
+              "color",
+              "quantity",
+              "price",
+            ];
+            requiredSuitFields.forEach((suit_field) => {
+              if (!suit[suit_field]) {
+                missingFields.push(`${suit_field} for suit ${index + 1}`);
+              }
+            });
+          });
+        }
+      });
+      if (missingFields.length > 0)
+        throw new Error(`Missing Fields ${missingFields}`);
+      const branch = await BranchModel.findOne({ _id: branchId });
+      if (!branch) throw new Error("Branch Not Found");
+
+      //DEDUCTING BAGS OR BOXES FROM STOCK
+      const bagsorBoxStock = await BagsAndBoxModel.findById(
+        packaging.id
+      ).session(session);
+      if (!bagsorBoxStock) throw new Error("Packaging Not Found");
+      const updatedBagsorBoxQuantity =
+        bagsorBoxStock.totalQuantity - parseInt(packaging.quantity);
+      if (updatedBagsorBoxQuantity < 0)
+        throw new Error(`Not Enough ${bagsorBoxStock.name} in Stock`);
+      bagsorBoxStock.totalQuantity = updatedBagsorBoxQuantity;
+      // await bagsorBoxStock.save({ session });
+
+      //DEDUCTING SUITS FROM STOCK
+      const suitsIdsToDeduct = suits_data.map((suit) => suit.id);
+      const suitsStock = await SuitsModel.find({
+        _id: { $in: suitsIdsToDeduct },
+      }).session(session);
+      const updatePromises = suitsStock.map((suit) => {
+        const suitToUpdate = suits_data.find((item) => item.id == suit._id);
+        const updatedSuitQuantity = suit.quantity - suitToUpdate.quantity;
+        if (updatedSuitQuantity < 0)
+          throw new Error(
+            `Not enough stock for suit with Design No: ${suit.d_no}`
+          );
+        suit.quantity = updatedSuitQuantity;
+        // return suit.save({ session });
+      });
+      // await Promise.all(updatePromises);
+
+      //ADDING IN DAILY SALE
+      const dailySaleForToday = await DailySaleModel.findOne({
+        branchId,
+        date: { $eq: date },
+      }).session(session);
+      if (!dailySaleForToday) {
+        throw new Error("Daily sale record not found for This Date");
+      }
+
+      //calculating profit
+      let TotalProfit = 0;
+      suits_data.forEach((suit) => {
+        const suitInStock = suitsStock.find((item) => item._id == suit.id);
+        const profitOnSale = suit.price - suitInStock.cost_price;
+        TotalProfit += profitOnSale * suit.quantity;
+        if (profitOnSale < 0)
+          throw new Error("Sale Price Must Be Greater Then Cost Price");
+      });
+
+      const updatedSaleData = {
+        ...dailySaleForToday.saleData,
+        payment_Method: (dailySaleForToday.saleData[payment_Method] += paid),
+        totalSale: (dailySaleForToday.saleData.totalSale += paid),
+        todayBuyerCredit: (dailySaleForToday.saleData.todayBuyerCredit += paid),
+        todayBuyerDebit: (dailySaleForToday.saleData.todayBuyerDebit +=
+
+          remaining),
+        totalProfit: (dailySaleForToday.saleData.totalProfit += TotalProfit),
+        totalCash:dailySaleForToday.saleData.totalCash += paid
+      };
+
+      dailySaleForToday.saleData = updatedSaleData;
+      // await dailySaleForToday.save({ session });
+
+      //GETTING BUYERS PREVIOUS DATA
+      const buyerData = await BuyersModel.findById({_id:buyerId});
+      console.log(buyerData);
+      if (!buyerData)  throw new Error("Buyer Data Not Found");
+
+
+      //DATA FOR VIRTUAL ACCOUNT
+      const new_total_debit = buyerData.virtual_account.total_debit + remaining;
+      const new_total_credit = buyerData.virtual_account.total_credit + paid;
+      const new_total_balance = buyerData.virtual_account.total_balance + remaining;
+      let new_status = "";
+
+      switch (true) {
+        case new_total_balance === 0:
+          new_status = "Paid";
+          break;
+        case new_total_balance === new_total_debit &&  new_total_credit > 0:
+          new_status = "Partially Paid";
+          break;
+        case new_total_credit === 0 && new_total_balance === new_total_debit:
+          new_status = "Unpaid";
+          break;
+      }
+      
+      const virtualAccountData = {
+        total_debit:new_total_debit,
+        total_credit:new_total_credit,
+        total_balance:new_total_balance,
+        status:new_status
+      };
+
+      //DATA FOR CREDIT DEBIT HISTORY
+
+      const credit_debit_history_details = [
+        {
+          date,
+          particular: `Bill No ${serialNumber}`,
+          debit: total,
+          balance: total,
+        },
+      ];
+      if (paid > 0) {
+        credit_debit_history_details.push({
+          date,
+          particular: payment_Method,
+          credit: paid,
+          balance: remaining,
+        });
+      }
+
+      //CREATING BUYER
+      // await BuyersModel.create(
+      //   [
+      //     {
+      //       branchId,
+      //       serialNumber,
+      //       name,
+      //       city,
+      //       cargo,
+      //       phone,
+      //       date,
+      //       bill_by,
+      //       payment_Method,
+      //       packaging,
+      //       discount,
+      //       suits_data,
+      //       virtual_account: virtualAccountData,
+      //       credit_debit_history: credit_debit_history_details,
+      //     },
+      //   ],
+      //   { session }
+      // );
+
+      return res
+        .status(200)
+        .json({ succes: true, message: "Bill Generated Successfully" });
+    });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  } finally {
+    session.endSession();
+  }
+}
 
 
--- backend/routes/Stock/LaceRoutes.js
+++ backend/routes/Stock/LaceRoutes.js
@@ -1,12 +1,13 @@
 
 import express from "express";
 import { superAdminAndAdminOnly } from "../../middleware/Auth.js";
-import { addLaceInStock, getAllLaceStock } from "../../controllers/Stock/LaceController.js";
+import { addLaceInStock, getAllLaceForEmbroidery, getAllLaceStock } from "../../controllers/Stock/LaceController.js";
 
 
 const laceRouter = express.Router();
 
 laceRouter.post("/addLaceInStock",superAdminAndAdminOnly, addLaceInStock);
 laceRouter.post("/getAllLaceStock",superAdminAndAdminOnly, getAllLaceStock);
+laceRouter.post("/getAllLaceForEmbroidery",superAdminAndAdminOnly, getAllLaceForEmbroidery);
 
 export default laceRouter;
