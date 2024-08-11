fc378a96ec35448b50be8fcde2c0edda7450e051
Suheer Zahid <150055579+CodeWithSuheer@users.noreply.github.com>
2024-08-11 06:38:49 +0500

    suheer commit

--- frontend/src/features/GenerateBillSlice.jsx
+++ frontend/src/features/GenerateBillSlice.jsx
@@ -4,11 +4,52 @@ import toast from "react-hot-toast";
 
 //API URL
 const generateBuyerBillUrl = "/api/buyers/generateBuyersBillandAddBuyer";
+const generateOldBuyerBillUrl = "/api/buyers/generateBillForOldbuyer";
+const getSuitFromDesignUrl = "/api/buyers/validateD_NoAndGetSuitData";
+const validateOldBuyerUrl = "/api/buyers/validateAndGetOldBuyerData";
 
 // GENERATE BUYER BILL THUNK
 export const generateBuyerBillAsync = createAsyncThunk("buyerBill/generate", async (billData) => {
     try {
         const response = await axios.post(generateBuyerBillUrl, billData);
+        toast.success(response.data.message)
+        return response.data;
+    } catch (error) {
+        toast.error(error.response.data.error);
+        console.log(error?.response?.data?.error);
+    }
+}
+);
+
+// GENERATE BILL FOR OLDER BUYER THUNK
+export const generateBillForOlderBuyerAsync = createAsyncThunk("OldbuyerBill/generate", async (billData) => {
+    try {
+        const response = await axios.post(generateOldBuyerBillUrl, billData);
+        toast.success(response.data.message)
+        return response.data;
+    } catch (error) {
+        toast.error(error.response.data.error);
+        console.log(error?.response?.data?.error);
+    }
+}
+);
+
+// GET SUIT FROM DESIGN NO THUNK
+export const getSuitFromDesignAsync = createAsyncThunk("buyerBill/getSuit", async (name) => {
+    try {
+        const response = await axios.post(getSuitFromDesignUrl, name);
+        return response.data;
+    } catch (error) {
+        toast.error(error.response.data.error);
+        console.log(error?.response?.data?.error);
+    }
+}
+);
+
+// GET OLD BUYER DATA THUNK
+export const validateOldBuyerAsync = createAsyncThunk("buyer/validate", async (name) => {
+    try {
+        const response = await axios.post(validateOldBuyerUrl, name);
         return response.data;
     } catch (error) {
         toast.error(error.response.data.error);
@@ -18,10 +59,13 @@ export const generateBuyerBillAsync = createAsyncThunk("buyerBill/generate", asy
 );
 
 
-
 // INITIAL STATE
 const initialState = {
     BuyerBill: [],
+    OldBuyerBilData: [],
+    SuitFromDesign: [],
+    OldBuyerData: [],
+    getBuyerLoading: false,
     loading: false,
 };
 
@@ -38,6 +82,33 @@ const GenerateBillSlice = createSlice({
             .addCase(generateBuyerBillAsync.fulfilled, (state, action) => {
                 state.loading = false;
                 state.BuyerBill = action.payload
+            })
+
+            // GENERATE BILL FOR OLDER BUYER
+            .addCase(generateBillForOlderBuyerAsync.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(generateBillForOlderBuyerAsync.fulfilled, (state, action) => {
+                state.loading = false;
+                state.OldBuyerBilData = action.payload
+            })
+
+            // GET SUIT FROM DESIGN NO
+            .addCase(getSuitFromDesignAsync.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(getSuitFromDesignAsync.fulfilled, (state, action) => {
+                state.loading = false;
+                state.SuitFromDesign = action.payload
+            })
+
+            // GET OLD BUYER DATA
+            .addCase(validateOldBuyerAsync.pending, (state) => {
+                state.getBuyerLoading = true;
+            })
+            .addCase(validateOldBuyerAsync.fulfilled, (state, action) => {
+                state.getBuyerLoading = false;
+                state.OldBuyerData = action.payload
             })
     },
 });
--- frontend/src/features/InStockSlice.jsx
+++ frontend/src/features/InStockSlice.jsx
@@ -186,7 +186,7 @@ export const GetAllBranches = createAsyncThunk("Branches/GetAll", async (id) => 
   try {
     const response = await axios.post(getAllBranches, id);
     // toast.success(response.data.message);
-    console.log(response.data);
+    // console.log(response.data);
     return response.data;
   } catch (error) {
     console.log(error.response.data.error);
