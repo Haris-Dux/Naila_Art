2adab7cb2e10c65c7cbe65495bfed0fbbcc676d2
Haris <HarisSaeed583@gmail.com>
2024-08-20 22:42:48 +0500

    fixed authorization for branch dashboard data

--- backend/controllers/Dashboard Data/DashboardDataController.js
+++ backend/controllers/Dashboard Data/DashboardDataController.js
@@ -14,6 +14,7 @@ export const getDashBoardDataForBranch = async (req, res, next) => {
       return res.status(403).send({ message: "Please Login Again" });
     }
     const user = await UserModel.findById({ _id: id });
+    if(!user.branchId) throw new Error("No branch Data For Authorized User")
     const branch = await BranchModel.findById({ _id: user.branchId });
     if (!branch) {
       return res
@@ -207,39 +208,39 @@ export const getDashBoardDataForBranch = async (req, res, next) => {
 
     //MOTHLY SALES DATA FOR GRAPH
     const salesForEveryMonth = await DailySaleModel.aggregate([
-        {
-          $match: {
-            branchId: user.branchId,
-            date: { $regex: `^${currentyear}` } 
-          }
+      {
+        $match: {
+          branchId: user.branchId,
+          date: { $regex: `^${currentyear}` },
         },
-        {
-          $group: {
-            _id: { 
-              month: { $substr: ["$date", 5, 2] } 
-            },
-            totalSale: { $sum: "$saleData.totalSale" } 
-          }
+      },
+      {
+        $group: {
+          _id: {
+            month: { $substr: ["$date", 5, 2] },
+          },
+          totalSale: { $sum: "$saleData.totalSale" },
         },
-        {
-          $sort: { 
-            "_id.month": 1 
-          }
+      },
+      {
+        $sort: {
+          "_id.month": 1,
         },
-        {
-          $project: {
-            month: "$_id.month", 
-            totalSale: 1,
-            _id: 0
-          }
-        }
-      ]);
-
+      },
+      {
+        $project: {
+          month: "$_id.month",
+          totalSale: 1,
+          _id: 0,
+        },
+      },
+    ]);
 
     //TOTAL SUITS DATA
-    const projection = "category quantity color"
-    const totalSuitsData = await SuitsModel.find({},projection).sort({createdAt : -1});
-
+    const projection = "category quantity color";
+    const totalSuitsData = await SuitsModel.find({}, projection).sort({
+      createdAt: -1,
+    });
 
     let dashBoardData = {
       salesBylocation: saleLocationData,
@@ -251,7 +252,7 @@ export const getDashBoardDataForBranch = async (req, res, next) => {
       monthlysale: monthlySaleData,
       grossSale: yearlySaleData,
       grossProfit: yearlyGrossProfitData,
-      monthlyGraphData:salesForEveryMonth
+      monthlyGraphData: salesForEveryMonth,
     };
 
     return res.status(200).json(dashBoardData);
@@ -261,255 +262,288 @@ export const getDashBoardDataForBranch = async (req, res, next) => {
 };
 
 export const getDashBoardDataForSuperAdmin = async (req, res, next) => {
-    try {
-    
-      //RECIEVEABLE AND SALES BY LOCATION DATA
-  
-      const buyers = await BuyersModel.find({ });
-      let recieveableSum = 0;
-      let saleLocationData = [];
-      buyers.forEach((buyer) => {
-        recieveableSum += buyer.virtual_account.total_balance;
-        const verifyCity = saleLocationData.find(
-          (item) => item.city.name.toLowerCase() === buyer.city.toLowerCase()
-        );
-        if (!verifyCity) {
-          saleLocationData.push({
-            city: {
-              name: buyer.city,
-              value: 1,
-            },
-          });
-        } else {
-          verifyCity.city.value += 1;
-        }
-      });
+  try {
+    //RECIEVEABLE AND SALES BY LOCATION DATA
 
-        //PAYABLE DATA
-
-        const sellers = await SellersModel.find({ });
-        let payableSum = 0;
-        sellers.forEach((seller) => {  
-          payableSum += seller.virtual_account.total_balance;
+    const buyers = await BuyersModel.find({});
+    let recieveableSum = 0;
+    let saleLocationData = [];
+    buyers.forEach((buyer) => {
+      recieveableSum += buyer.virtual_account.total_balance;
+      const verifyCity = saleLocationData.find(
+        (item) => item.city.name.toLowerCase() === buyer.city.toLowerCase()
+      );
+      if (!verifyCity) {
+        saleLocationData.push({
+          city: {
+            name: buyer.city,
+            value: 1,
+          },
         });
-  
-      //SALES DATA , BANK DATA AND CASH IN HAND
-  
-      //daily sale and difference from yesterday
+      } else {
+        verifyCity.city.value += 1;
+      }
+    });
 
-      const today = moment.tz("Asia/Karachi").format("YYYY-MM-DD");
-      const yesterday = moment
+    //PAYABLE DATA
+
+    const sellers = await SellersModel.find({});
+    let payableSum = 0;
+    sellers.forEach((seller) => {
+      payableSum += seller.virtual_account.total_balance;
+    });
+
+    //SALES DATA , BANK DATA AND CASH IN HAND
+
+    //daily sale and difference from yesterday
+
+    const today = moment.tz("Asia/Karachi").format("YYYY-MM-DD");
+    const yesterday = moment
       .tz("Asia/Karachi")
       .subtract(1, "day")
       .format("YYYY-MM-DD");
 
-      //Today's total sales
-      const [todaySalesResult] = await DailySaleModel.aggregate([
-        { $match: { date: today } },
-      { $facet:{
-        totalSale:[ { $group: { _id: null, totalSale: { $sum: "$saleData.totalSale" } } }],
-        cashInMeezanBank:[ { $group: { _id: null, cashInMeezanBank: { $sum: "$saleData.cashInMeezanBank" } } }],
-        cashInJazzCash:[ { $group: { _id: null, cashInJazzCash: { $sum: "$saleData.cashInJazzCash" } } }],
-        cashInEasyPaisa:[ { $group: { _id: null, cashInEasyPaisa: { $sum: "$saleData.cashInEasyPaisa" } } }],
-        totalCash:[ { $group: { _id: null, totalCash: { $sum: "$saleData.totalCash" } } }],
-       }}
-      ]);
-    
-      //Yesterday's total sales
-      const [yesterdaySalesResult] = await DailySaleModel.aggregate([
-        { $match: { date: yesterday } },
-        { $group: { _id: null, totalSale: { $sum: "$saleData.totalSale" } } }
-      ]);
+    //Today's total sales
+    const [todaySalesResult] = await DailySaleModel.aggregate([
+      { $match: { date: today } },
+      {
+        $facet: {
+          totalSale: [
+            {
+              $group: { _id: null, totalSale: { $sum: "$saleData.totalSale" } },
+            },
+          ],
+          cashInMeezanBank: [
+            {
+              $group: {
+                _id: null,
+                cashInMeezanBank: { $sum: "$saleData.cashInMeezanBank" },
+              },
+            },
+          ],
+          cashInJazzCash: [
+            {
+              $group: {
+                _id: null,
+                cashInJazzCash: { $sum: "$saleData.cashInJazzCash" },
+              },
+            },
+          ],
+          cashInEasyPaisa: [
+            {
+              $group: {
+                _id: null,
+                cashInEasyPaisa: { $sum: "$saleData.cashInEasyPaisa" },
+              },
+            },
+          ],
+          totalCash: [
+            {
+              $group: { _id: null, totalCash: { $sum: "$saleData.totalCash" } },
+            },
+          ],
+        },
+      },
+    ]);
 
-      const dailySaleData = {
-        today: todaySalesResult.totalSale[0].totalSale,
-        differenceFromYesterday: todaySalesResult.totalSale[0].totalSale - yesterdaySalesResult.totalSale,
-      }; 
+    //Yesterday's total sales
+    const [yesterdaySalesResult] = await DailySaleModel.aggregate([
+      { $match: { date: yesterday } },
+      { $group: { _id: null, totalSale: { $sum: "$saleData.totalSale" } } },
+    ]);
 
-      //monthly and yearly sale and difference from previous month and year
-      const currentMonth = moment.tz("Asia/Karachi").format("YYYY-MM");
-      const currentyear = moment.tz("Asia/Karachi").format("YYYY");
-      const previousYear = moment
-        .tz("Asia/Karachi")
-        .subtract(1, "year")
-        .format("YYYY");
-      const previousMonth = moment
-        .tz("Asia/Karachi")
-        .subtract(1, "month")
-        .format("YYYY-MM");
-      const currentMonthAndYearSale = await DailySaleModel.aggregate([
-        {
-          $match: {
-            date: { $regex: `^${currentyear}` },
-          },
+    const dailySaleData = {
+      today: todaySalesResult.totalSale[0].totalSale,
+      differenceFromYesterday:
+        todaySalesResult.totalSale[0].totalSale -
+        yesterdaySalesResult.totalSale,
+    };
+
+    //monthly and yearly sale and difference from previous month and year
+    const currentMonth = moment.tz("Asia/Karachi").format("YYYY-MM");
+    const currentyear = moment.tz("Asia/Karachi").format("YYYY");
+    const previousYear = moment
+      .tz("Asia/Karachi")
+      .subtract(1, "year")
+      .format("YYYY");
+    const previousMonth = moment
+      .tz("Asia/Karachi")
+      .subtract(1, "month")
+      .format("YYYY-MM");
+    const currentMonthAndYearSale = await DailySaleModel.aggregate([
+      {
+        $match: {
+          date: { $regex: `^${currentyear}` },
         },
-        {
-          $facet: {
-            yearlyGrossSale: [
-              {
-                $group: {
-                  _id: null,
-                  totalSale: { $sum: "$saleData.totalSale" },
-                },
+      },
+      {
+        $facet: {
+          yearlyGrossSale: [
+            {
+              $group: {
+                _id: null,
+                totalSale: { $sum: "$saleData.totalSale" },
               },
-            ],
-            yearlyGrossProfit: [
-              {
-                $group: {
-                  _id: null,
-                  totalProfit: { $sum: "$saleData.totalProfit" },
-                },
+            },
+          ],
+          yearlyGrossProfit: [
+            {
+              $group: {
+                _id: null,
+                totalProfit: { $sum: "$saleData.totalProfit" },
               },
-            ],
-            monthlyGrossSale: [
-              {
-                $match: {
-                  date: { $regex: `^${currentMonth}` },
-                },
+            },
+          ],
+          monthlyGrossSale: [
+            {
+              $match: {
+                date: { $regex: `^${currentMonth}` },
               },
-              {
-                $group: {
-                  _id: null,
-                  totalSale: { $sum: "$saleData.totalSale" },
-                },
+            },
+            {
+              $group: {
+                _id: null,
+                totalSale: { $sum: "$saleData.totalSale" },
               },
-            ],
-          },
+            },
+          ],
         },
-      ]);
-  
-      const previousMonthAndYearlysale = await DailySaleModel.aggregate([
-        {
-          $facet: {
-            yearlyGrossSale: [
-              {
-                $match: {
-                  date: { $regex: `^${previousYear}` },
-                },
+      },
+    ]);
+
+    const previousMonthAndYearlysale = await DailySaleModel.aggregate([
+      {
+        $facet: {
+          yearlyGrossSale: [
+            {
+              $match: {
+                date: { $regex: `^${previousYear}` },
               },
-              {
-                $group: {
-                  _id: null,
-                  totalSale: { $sum: "$saleData.totalSale" },
-                },
+            },
+            {
+              $group: {
+                _id: null,
+                totalSale: { $sum: "$saleData.totalSale" },
               },
-            ],
-            yearlyGrossProfit: [
-              {
-                $match: {
-                  date: { $regex: `^${previousYear}` },
-                },
+            },
+          ],
+          yearlyGrossProfit: [
+            {
+              $match: {
+                date: { $regex: `^${previousYear}` },
               },
-              {
-                $group: {
-                  _id: null,
-                  totalProfit: { $sum: "$saleData.totalProfit" },
-                },
+            },
+            {
+              $group: {
+                _id: null,
+                totalProfit: { $sum: "$saleData.totalProfit" },
               },
-            ],
-            monthlyGrossSale: [
-              {
-                $match: {
-                  date: { $regex: `^${previousMonth}` },
-                },
+            },
+          ],
+          monthlyGrossSale: [
+            {
+              $match: {
+                date: { $regex: `^${previousMonth}` },
               },
-              {
-                $group: {
-                  _id: null,
-                  totalSale: { $sum: "$saleData.totalSale" },
-                },
+            },
+            {
+              $group: {
+                _id: null,
+                totalSale: { $sum: "$saleData.totalSale" },
               },
-            ],
-          },
+            },
+          ],
         },
-      ]);
-  
-      //monthly sale data
-      const monthlySaleData = {
-        currentMonthSale:
-          currentMonthAndYearSale[0].monthlyGrossSale[0].totalSale,
-        differenceFromLastMonth:
-          currentMonthAndYearSale[0].monthlyGrossSale[0].totalSale -
-          previousMonthAndYearlysale[0].monthlyGrossSale[0].totalSale,
-      };
-  
-      //yearly sale data
-      const yearlySaleData = {
-        currentyearSale: currentMonthAndYearSale[0].yearlyGrossSale[0].totalSale,
-        differenceFromLastYear:
-          currentMonthAndYearSale[0].yearlyGrossSale[0].totalSale -
-          previousMonthAndYearlysale[0].yearlyGrossSale[0].totalSale,
-      };
-  
-      //yearly gross profit
-      const yearlyGrossProfitData = {
-        currentYearGrossProfit:
-          currentMonthAndYearSale[0].yearlyGrossProfit[0].totalProfit,
-        differenceFromLastyear:
-          currentMonthAndYearSale[0].yearlyGrossProfit[0].totalProfit -
-          previousMonthAndYearlysale[0].yearlyGrossProfit[0].totalProfit,
-      };
-  
-      //banks data
-      const banksData = {
-        Meezan_Bank: todaySalesResult.cashInMeezanBank[0].cashInMeezanBank,
-        JazzCash: todaySalesResult.cashInJazzCash[0].cashInJazzCash,
-        EasyPaisa: todaySalesResult.cashInEasyPaisa[0].cashInEasyPaisa,
-      };
-  
-      //cash in hand data
-      const cashInHandData = todaySalesResult.totalSale[0].totalCash; 
-  
-      //MOTHLY SALES DATA FOR GRAPH
-      const salesForEveryMonth = await DailySaleModel.aggregate([
-          {
-            $match: {
-              date: { $regex: `^${currentyear}` } 
-            }
+      },
+    ]);
+
+    //monthly sale data
+    const monthlySaleData = {
+      currentMonthSale:
+        currentMonthAndYearSale[0].monthlyGrossSale[0].totalSale,
+      differenceFromLastMonth:
+        currentMonthAndYearSale[0].monthlyGrossSale[0].totalSale -
+        previousMonthAndYearlysale[0].monthlyGrossSale[0].totalSale,
+    };
+
+    //yearly sale data
+    const yearlySaleData = {
+      currentyearSale: currentMonthAndYearSale[0].yearlyGrossSale[0].totalSale,
+      differenceFromLastYear:
+        currentMonthAndYearSale[0].yearlyGrossSale[0].totalSale -
+        previousMonthAndYearlysale[0].yearlyGrossSale[0].totalSale,
+    };
+
+    //yearly gross profit
+    const yearlyGrossProfitData = {
+      currentYearGrossProfit:
+        currentMonthAndYearSale[0].yearlyGrossProfit[0].totalProfit,
+      differenceFromLastyear:
+        currentMonthAndYearSale[0].yearlyGrossProfit[0].totalProfit -
+        previousMonthAndYearlysale[0].yearlyGrossProfit[0].totalProfit,
+    };
+
+    //banks data
+    const banksData = {
+      Meezan_Bank: todaySalesResult.cashInMeezanBank[0].cashInMeezanBank,
+      JazzCash: todaySalesResult.cashInJazzCash[0].cashInJazzCash,
+      EasyPaisa: todaySalesResult.cashInEasyPaisa[0].cashInEasyPaisa,
+    };
+
+    //cash in hand data
+    const cashInHandData = todaySalesResult.totalSale[0].totalCash;
+
+    //MOTHLY SALES DATA FOR GRAPH
+    const salesForEveryMonth = await DailySaleModel.aggregate([
+      {
+        $match: {
+          date: { $regex: `^${currentyear}` },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            month: { $substr: ["$date", 5, 2] },
           },
-          {
-            $group: {
-              _id: { 
-                month: { $substr: ["$date", 5, 2] } 
-              },
-              totalSale: { $sum: "$saleData.totalSale" } 
-            }
-          },
-          {
-            $sort: { 
-              "_id.month": 1 
-            }
-          },
-          {
-            $project: {
-              month: "$_id.month", 
-              totalSale: 1,
-              _id: 0
-            }
-          }
-        ]);
-  
-      //TOTAL SUITS DATA
-      const projection = "category quantity color"
-      const totalSuitsData = await SuitsModel.find({},projection).sort({createdAt : -1});
-  
-  
-      let dashBoardData = {
-        salesBylocation: saleLocationData,
-        bankAccountsData: banksData,
-        cashInHand: cashInHandData,
-        totalSuits: totalSuitsData,
-        recieveable: recieveableSum,
-        dailySale: dailySaleData,
-        monthlysale: monthlySaleData,
-        grossSale: yearlySaleData,
-        grossProfit: yearlyGrossProfitData,
-        monthlyGraphData:salesForEveryMonth,
-        payable:payableSum
-      };
-  
-      return res.status(200).json(dashBoardData);
-    } catch (error) {
-      return res.status(500).json({ error: error.message });
-    }
-  };
+          totalSale: { $sum: "$saleData.totalSale" },
+        },
+      },
+      {
+        $sort: {
+          "_id.month": 1,
+        },
+      },
+      {
+        $project: {
+          month: "$_id.month",
+          totalSale: 1,
+          _id: 0,
+        },
+      },
+    ]);
+
+    //TOTAL SUITS DATA
+    const projection = "category quantity color";
+    const totalSuitsData = await SuitsModel.find({}, projection).sort({
+      createdAt: -1,
+    });
+
+    let dashBoardData = {
+      salesBylocation: saleLocationData,
+      bankAccountsData: banksData,
+      cashInHand: cashInHandData,
+      totalSuits: totalSuitsData,
+      recieveable: recieveableSum,
+      dailySale: dailySaleData,
+      monthlysale: monthlySaleData,
+      grossSale: yearlySaleData,
+      grossProfit: yearlyGrossProfitData,
+      monthlyGraphData: salesForEveryMonth,
+      payable: payableSum,
+    };
+
+    return res.status(200).json(dashBoardData);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
