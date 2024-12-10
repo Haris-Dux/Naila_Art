95032e803243b26154d971223a22c1e83cc89da8
Haris-Dux <harissaeed583@gmail.com>
2024-12-10 17:07:54 +0500

    Completed expensxe functionality and added updated payment methods

--- backend/models/Stock/ExpenseModel.js
+++ backend/models/Stock/ExpenseModel.js
@@ -1,35 +1,35 @@
 import mongoose from "mongoose";
 
-const dataSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "name required"],
-  },
-  reason: {
-    type: String,
-    required: [true, "reason required"],
-  },
-  Date: {
-    type: Date,
-    required: [true, "Date required"],
-  },
-  rate: {
-    type: Number,
-    required: [true, "rate required"],
-  },
-  serial_no: {
-    type: Number,
-    required: [true, "serial_no required"],
-  },
-}, { timestamps: true });
-
 const expenseSchema = new mongoose.Schema(
   {
     branchId: {
-      type: mongoose.Types.ObjectId,
+      type: String,
       required: [true, "branchId required"],
     },
-    brannchExpenses: [dataSchema],
+    name: {
+      type: String,
+      required: [true, "name required"],
+    },
+    reason: {
+      type: String,
+      required: [true, "reason required"],
+    },
+    Date: {
+      type: String,
+      required: [true, "Date required"],
+    },
+    rate: {
+      type: Number,
+      required: [true, "rate required"],
+    },
+    serial_no: {
+      type: Number,
+      required: [true, "serial_no required"],
+    },
+    payment_Method:{
+      type: String,
+      default:"cashSale"
+    }
   },
   { timestamps: true }
 );
--- backend/routes/Stock/ExpenseRoutes.js
+++ backend/routes/Stock/ExpenseRoutes.js
@@ -1,14 +1,15 @@
 
 
 import express from "express";
-import { addExpense, getAllExpenses, getExpensesForBranch } from "../../controllers/Stock/ExpenseController.js";
-import { superAdminAndAdminOnly, verifyUser } from "../../middleware/Auth.js";
+import { addExpense, deleteExpense, getAllExpenses, getExpensesForBranch } from "../../controllers/Stock/ExpenseController.js";
+import { verifyUser } from "../../middleware/Auth.js";
 
 const expenseRouter = express.Router();
 
 expenseRouter.post("/addExpense",verifyUser,addExpense);
 expenseRouter.post("/getAllExpenses",verifyUser, getAllExpenses);
 expenseRouter.post("/getExpensesForBranch",verifyUser, getExpensesForBranch);
+expenseRouter.post("/deleteExpense",verifyUser, deleteExpense);
 
 
 export default expenseRouter;
