2a2789077372ae4fbd20e7da128d6cbef168ffdf
Haris-Dux <harissaeed583@gmail.com>
2025-04-05 07:25:47 +0500

    fixing enviroments for pipeline

--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -316,7 +316,7 @@ const Dashboard = () => {
                 className="hidden sm:flex items-center justify-between mr-4"
               >
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                  NAILA ARTS (DEV ENVIROMENT)
+                  NAILA ARTS (DEV-ENVIROMENT)
                 </span>
               </Link>
             </div>
