46f2f8051d22c4f3003121ce29a2df8e5741c4e6
Haris-Dux <harissaeed583@gmail.com>
2025-04-06 12:48:11 +0500

    process.env fix for viteconfig file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = process.env.VITE_API_URL;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
-  
 
   server: {
     
