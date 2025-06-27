250876a7664a206c73667f5986d1c2adbb69bfc8
Haris-Dux <harissaeed583@gmail.com>
2025-06-27 05:12:45 +0500

    fixed bundles issue in branch stock. Past total cah addition for delete expense

--- backend/utils/Common.js
+++ backend/utils/Common.js
@@ -1,11 +1,16 @@
-
 import moment from "moment-timezone";
 
 export const getTodayDate = () => {
-  return moment().tz('Asia/Karachi').format("YYYY-MM-DD");
+  return moment().tz("Asia/Karachi").format("YYYY-MM-DD");
 };
 
-
-  
-
-  
+export const verifyPastDate = (date) => {
+  const futureDate = moment.tz(date, "Asia/Karachi").startOf("day");
+  const now = moment.tz("Asia/Karachi").startOf("day");
+  const isFutureDate = futureDate.isAfter(now);
+  if (isFutureDate) {
+    throw new Error("Date cannot be in the future");
+  }
+  const pastDate = futureDate.isBefore(now);
+  return pastDate;
+};
