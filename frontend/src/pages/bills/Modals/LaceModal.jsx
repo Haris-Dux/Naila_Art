2e592b18c3afa4cb43878b0a9ccff54b36d5e605
Haris-Dux <harissaeed583@gmail.com>
2026-06-11 14:24:21 +0500

    fixed change able date in seller bill forms

--- frontend/src/pages/bills/Modals/LaceModal.jsx
+++ frontend/src/pages/bills/Modals/LaceModal.jsx
@@ -300,7 +300,6 @@ const LaceModal = ({ isOpen, closeModal, sellerDetails }) => {
                       onChange={handleChange}
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-0 focus:border-gray-300 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                       required
-                      readOnly
                     />
                   </div>
 
