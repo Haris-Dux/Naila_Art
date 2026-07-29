349b9a80d1d454ee5309a8e424beb776635542f5
Haris-Dux <harissaeed583@gmail.com>
2026-07-29 11:56:31 +0500

    other sale bill delete endpoint and integration.

--- backend/routes/OtherSaleRoutes.js
+++ backend/routes/OtherSaleRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { superAdminAndAdminOnly } from "../middleware/Auth.js";
 import {
+  deleteOtherSaleBill,
   generateOtherSaleBill,
   getAllOtherSaleBills,
 } from "../controllers/OtherSaleController.js";
@@ -16,6 +17,11 @@ otherSaleRouter.post(
   "/getAllOtherSaleBills",
   superAdminAndAdminOnly,
   getAllOtherSaleBills
+);
+otherSaleRouter.post(
+  "/deleteOtherSaleBill",
+  superAdminAndAdminOnly,
+  deleteOtherSaleBill
 );
 
 
